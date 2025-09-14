d74c8ce84623f21e00abdd3c9ddaded3b7dc8311
David Zeff <dpzeff@gmail.com>
2025-09-14 15:02:02 +0300

    .

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,52 @@
 "use client";
 import Link from "next/link";
-import { Menu } from "lucide-react";
-import { LogIn } from "lucide-react";
+import Image from "next/image";
 import { useState } from "react";
-import { Logs } from "lucide-react";
+import { Menu, Pencil, LogIn, Logs, UserPen } from "lucide-react";
 
 export default function Home() {
   const [isMenuClicked, setIsMenuClicked] = useState(false);
   return (
     <>
-      <div className="bg-gray-200 h-48 flex justify-evenly items-center gap-5">
-        <form className="h-fit" action="/cities" method="GET">
+      <div className="bg-gray-200 h-48 flex justify-evenly items-center gap-5 border-b-2 border-gray-300 shadow">
+        <Link href="/" className="hidden sm:block">
+          <Image
+            src={"/logos/logo.png"}
+            alt={"Logo Image"}
+            width={100}
+            height={100}
+            className="object-fit"
+          />
+        </Link>
+
+        <form className="h-fit w-1/4" action="/cities" method="GET">
           <input
-            className="bg-white rounded-xl h-8 p-2"
-            placeholder="search city"
+            className="bg-white border-2 border-gray-300 shadow rounded-xl h-8 p-4 w-full"
+            placeholder="Search City"
             type="search"
           />
         </form>
-        <div className="relative">
+
+        <div className="hidden md:flex justify-center gap-30 w-fit">
+          <Link
+            className="bg-white border-2 border-gray-300 shadow rounded-xl hover:bg-gray-100 text-center p-2 flex justify-evenly gap-2"
+            href={"/profile"}
+          >
+            Profile
+            <UserPen />
+          </Link>
+          <Link
+            className="bg-white border-2 border-gray-300 shadow rounded-xl hover:bg-gray-100 text-center p-2 flex justify-evenly gap-2"
+            href={"/register"}
+          >
+            Register
+            <Pencil />
+          </Link>
+        </div>
+        <button className="bg-green-100 p-2 border-2 border-gray-300 shadow rounded-full hover:cursor-pointer hover:bg-green-200">
+          <LogIn></LogIn>
+        </button>
+        <div className="relative md:hidden">
           <button
             onClick={() => setIsMenuClicked(!isMenuClicked)}
             className="bg-white p-2 rounded-full hover:cursor-pointer hover:bg-gray-100"
@@ -41,9 +70,6 @@ export default function Home() {
             </div>
           )}
         </div>
-        <button className="bg-green-100 p-2 rounded-full hover:cursor-pointer hover:bg-green-200">
-          <LogIn></LogIn>
-        </button>
       </div>
     </>
   );
